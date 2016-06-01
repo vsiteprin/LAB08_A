f0b32288a67645b23458872ec97e6d511fc9ec62
Domagoj Deduš <domagoj.dedus@vsite.hr>
2016-06-01 18:51:34 +0200

    lab08 treci commit

---
+++ dd.s
@@ -0,0 +1,26 @@
+	.file	"dd.c"
+	.section	.rodata
+.LC0:
+	.string	"Domagoj Dedu\305\241"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	$.LC0, %edi
+	call	puts
+	movl	$0, %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 5.2.1-22ubuntu2) 5.2.1 20151010"
+	.section	.note.GNU-stack,"",@progbits
